689b247c187a1156a5b79c9d47b4fa70da743105
padamaszek <ursus707@gmail.com>
2016-06-07 23:13:40 +0200

    app component add

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
-})
-export class AppComponent { }
-
-
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
+    import { Component } from '@angular/core';
+    export class Hero {
+      id: number;
+      name: string;
+    }
+    @Component({
+      selector: 'my-app',
+      template:`
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+          <label>name: </label>
+          <input [(ngModel)]="hero.name" placeholder="name">
+        </div>
+        `
+    })
+    export class AppComponent {
+      title = 'Tour of Heroes';
+      hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+      };
+    }
